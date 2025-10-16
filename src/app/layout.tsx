60e93577f8f1bec5634e81a26cfbbe86e565cd17
Nagy Stefan Szilamer <pityuka@gmail.com>
2025-10-16 22:48:47 +0300

    gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,5 +11,26 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return children;
+  return (
+    <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-2W5D1FJ4QV"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-2W5D1FJ4QV');
+            `,
+          }}
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
 }
